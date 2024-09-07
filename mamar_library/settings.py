2716ae435d44317ae87b2b4cc02e2b97d6a0379a
khurshida333 <akhurshida70@gmail.com>
2024-09-07 10:58:10 +0600

    domain changed

--- mamar_library/settings.py
+++ mamar_library/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-assignment-5-2ulk.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
